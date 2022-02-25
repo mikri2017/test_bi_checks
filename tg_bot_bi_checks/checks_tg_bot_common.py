ec737cfacd963d977f584fbc0f7bfba404c4bf73
MIKRI <2007mik007@mail.ru>
2022-02-25 11:38:33 +0700

    Прописал функции внесения продажи и получения статистики через http сервер -  add_check() и get_check_stat(), соответственно

--- tg_bot_bi_checks/checks_tg_bot_common.py
+++ tg_bot_bi_checks/checks_tg_bot_common.py
@@ -1,3 +1,5 @@
+import requests
+
 def check_tg_upd_data(upd_inf):
     res = {
         'type': "",
@@ -35,3 +37,52 @@ def send_bot_hello_msg(tg_bot, chat_id):
     ]
 
     return tg_bot.send_msg(chat_id, msg, buttons)
+
+
+def add_check(bi_checks_host, sum):
+    res = {
+        'res': "ok"
+    }
+
+    url = "http://%s/add_check?sum=%s" % (bi_checks_host, str(sum))
+
+    try:
+        req_res = requests.get(url)
+        answer = req_res.json()
+        if answer['res'] == "ok":
+            res['sum'] = answer['sum']
+        else:
+            res['res'] = "err"
+            res['msg'] = answer['msg']
+            return res
+    except Exception as ex:
+        res['res'] = "err"
+        res['msg'] = "Ошибка получения URL [%s]: %s" % (url, str(ex))
+
+    return res
+
+
+def get_check_stat(bi_checks_host, date_begin, date_end):
+    res = {
+        'res': "ok"
+    }
+
+    url = "http://%s/get_sale_stat?date_begin=%s&date_end=%s" % (
+        bi_checks_host,
+        date_begin.strftime("%Y-%m-%d"),
+        date_end.strftime("%Y-%m-%d")
+    )
+
+    try:
+        req_res = requests.get(url)
+        answer = req_res.json()
+        if answer['res'] == "ok":
+            res['items'] = answer['items']
+        else:
+            res['res'] = "err"
+            res['msg'] = answer['msg']
+    except Exception as ex:
+        res['res'] = "err"
+        res['msg'] = "Ошибка получения URL [%s]: %s" % (url, str(ex))
+
+    return res
