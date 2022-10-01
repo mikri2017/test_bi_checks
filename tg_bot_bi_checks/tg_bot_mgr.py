38fa1adbbbcb0636208b230211f68489d8033e51
MIKRI <2007mik007@mail.ru>
2022-10-01 14:57:19 +0700

    В класс TgBotMgr - добавил отправку группы фотографий - изменил код отправки текстового сообщение под использование словарей

--- tg_bot_bi_checks/tg_bot_mgr.py
+++ tg_bot_bi_checks/tg_bot_mgr.py
@@ -1,5 +1,7 @@
+import os
 import json
 import requests
+from io import BytesIO
 import urllib3
 
 class TgBotMgr():
@@ -66,16 +68,25 @@ class TgBotMgr():
     def send_msg(self, chat_id, msg, buttons = []):
         """Отправка сообщения
 
-            :param chat_id: ID чата для отправки
-            :param msg: Текст сообщения
-            :param buttons: Массив словарей для добавления к сообщению кнопок,
-                            поля 'text' - подпись кнопки,
-                            'callback_data' - идентификатор до 64 байт, для
-                            опознания запрошенного пользователем действия
-            :return: Словарь результатов
+        :param chat_id: ID чата для отправки
+        :param msg: Текст сообщения
+        :param buttons: Массив словарей для добавления к сообщению кнопок,
+                        поля 'text' - подпись кнопки,
+                        'callback_data' - идентификатор до 64 байт, для
+                        опознания запрошенного пользователем действия
+        :return: Словарь результатов или False при ошибке
         """
 
-        params = "chat_id=%i&text=%s" % (chat_id, msg)
+        url = "%s%s/sendMessage" % (
+            self.__tg_api_url,
+            self.__token
+        )
+
+        params = {
+            'chat_id': chat_id,
+            'parse_mode': "HTML",
+            'text': msg
+        }
 
         inl_buttons = {
             'inline_keyboard': [
@@ -83,16 +94,10 @@ class TgBotMgr():
             ]
         }
 
-        params += "&reply_markup=" + json.dumps(inl_buttons)
-
-        url = "%s%s/sendMessage?%s" % (
-            self.__tg_api_url,
-            self.__token,
-            params
-        )
+        params['reply_markup'] = json.dumps(inl_buttons)
 
         try:
-            res = requests.get(url, verify=False)
+            res = requests.get(url, params=params, verify=False)
             res = res.json()
             if res['ok'] == True:
                 return res['result']
@@ -135,6 +140,7 @@ class TgBotMgr():
             self.__error_mgr = "Ошибка получения URL [%s]: %s" % (url, str(ex))
             return False
 
+
     def send_photo(self, chat_id, file_path, caption = ""):
         url = "%s%s/sendPhoto" % (
             self.__tg_api_url,
@@ -159,6 +165,71 @@ class TgBotMgr():
                             file_path, res['error_code'], res['description']
                         )
                     return False
+        except Exception as ex:
+            self.__error_mgr = "Ошибка получения URL [%s]: %s" % (url, str(ex))
+            return False
+
+
+    def send_media_group_photos(self, chat_id, paths_with_captions):
+        """Отправка группы фотографий в виде альбома
+        :param chat_id: ID чата для отправки
+        :param paths_with_captions: Словарь "Подпись": "Путь к файлу"
+        :return: Словарь результатов или False при ошибке
+        """
+
+        url = "%s%s/sendMediaGroup" % (
+            self.__tg_api_url,
+            self.__token
+        )
+    
+        files = {}
+        media = []
+
+        msg = ""
+        first = True
+        try:
+            for caption, path in paths_with_captions.items():
+                f_img = open(path, "rb")
+                with BytesIO(f_img.read()) as b_out:
+                    b_out.seek(0)
+                    name = os.path.basename(path)
+                    files[name] = b_out.read()
+                    media.append(
+                        dict(
+                            type='photo',
+                            media=f'attach://{name}',
+                            parse_mode="HTML",
+                            caption=caption
+                        )
+                    )
+
+                if first:
+                    msg += caption
+                    first = False
+                else:
+                    msg += "\n%s" % (caption)
+        except Exception as ex:
+            self.__error_mgr = "Ошибка получения URL [%s]: %s" % (url, str(ex))
+            return False
+
+        params = {
+            'chat_id': chat_id,
+            'media': json.dumps(media),
+            'parse_mode': "HTML",
+            'caption': msg
+        }
+
+        try:
+            res = requests.post(url, data=params, files=files, verify=False)
+            res = res.json()
+            if res['ok'] == True:
+                return res['result']
+            else:
+                self.__error_mgr = "Ошибка от Telegram API при отправке" \
+                    + " группы фото: [%i] %s" % (
+                        res['error_code'], res['description']
+                    )
+                return False
         except Exception as ex:
             self.__error_mgr = "Ошибка получения URL [%s]: %s" % (url, str(ex))
             return False
